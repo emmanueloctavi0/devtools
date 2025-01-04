a5898eaa64e30a89e790c61361dba34b72067aa5
Emmanuel Octavio <emmanueloctaviomc@gmail.com>
2025-01-03 23:27:29 -0600

    plain base64 encoded

--- src/codec/components/Textarea.jsx
+++ src/codec/components/Textarea.jsx
@@ -1,8 +1,12 @@
-export const Textarea = ({ title }) => {
+export const Textarea = ({ title, content, onChange }) => {
   return (
     <div className="my-5 text-xl">
       <h2 className="mb-3">{title}</h2>
-      <textarea className="border border-gray-500 w-3/4 h-36 rounded text-slate-900"></textarea>
+      <textarea
+        value={content}
+        onChange={onChange}
+        className="border border-gray-500 w-3/4 h-36 rounded text-slate-900"
+      ></textarea>
     </div>
   );
 };
--- src/codec/pages/Codec.jsx
+++ src/codec/pages/Codec.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import { Textarea } from "../components/Textarea";
 
 export const Codec = () => {
+  const [plainText, setPlainText] = useState("");
+  const [encoded, setEncoded] = useState("");
+
+  useEffect(() => {
+    setEncoded(btoa(plainText));
+  }, [plainText]);
+
   return (
     <>
       <div className="container mx-auto text-center">
         <h1 className="mt-12 text-3xl">Base64 Encoder</h1>
-        <Textarea title="Plain text" />
-        <Textarea title="Base64" />
+        <Textarea
+          title="Plain text"
+          content={plainText}
+          onChange={(e) => setPlainText(e.target.value)}
+        />
+        <Textarea
+          title="Base64"
+          content={encoded}
+          onChange={(e) => setEncoded(e.target.value)}
+        />
       </div>
     </>
   );
